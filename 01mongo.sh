3d02135ac7da22b3436d20df7408f870e36c8df7
Naveen <naveenallam99@gmail.com>
2024-08-25 16:36:52 +0530

    test08

--- 01mongo.sh
+++ 01mongo.sh
@@ -11,6 +11,7 @@ N="\e[0m"
 
 set -xe
 
+LOGFILE="/home/centos-$0.log"
 CHECK(){
     if [ $1 -ne 0 ]
     then
@@ -28,28 +29,28 @@ else
    echo -e " $G starting the installation... $N"
 fi   
 
-cp mongo.repo  /etc/yum.repos.d/mongo.repo
+cp mongo.repo  /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 CHECK $? "copying mongo repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>> $LOGFILE
 
 CHECK() $? "installing mongodb"
 
-systemctl enable mongod 
+systemctl enable mongod &>> $LOGFILE
 
 CHECK() $? "enabaling mongodb"
 
-systemctl start mongod
+systemctl start mongod &>> $LOGFILE
 
 CHECK() $? "started mongodb"
 
 #changing mongobd host ip using sed(streamline editor ) very powerful
 
-sed -i 's/127.0.0.1 to 0.0.0.0g' /etc/mongod.conf
+sed -i 's/127.0.0.1 to 0.0.0.0g' /etc/mongod.conf &>> $LOGFILE
 
 CHECK() $? " setting remote access to mongodb"
 
-systemctl restart mongod 
+systemctl restart mongod &>> $LOGFILE
 
 CHECK() $? "restarting mongodb"
