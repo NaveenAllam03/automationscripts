6f43ce6ab5a7ddbd677963682f87a3d015ac548f
Naveen <naveenallam99@gmail.com>
2024-08-25 16:02:09 +0530

    test01

---
+++ 01mongo.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# function to validate using exit status
+CHECK(){
+    if [ $1 -ne 0 ]
+    then
+       echo " $2 $R failed..$N "
+       exit 1 
+    else
+       echo " $2 $G sucessfull..$N"   
+    fi
+}
+
+user=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $user -ne 0 ]
+then
+   echo " $R switch to root user and run commands $N"
+   exit 1
+else
+   echo " $G starting the installation... $N"
+fi   
+
+cp mongo.repo  /etc/yum.repos.d/mongo.repo
+CHECK $? "copying mongo repo"
+
+dnf install mongodb-org -y 
+CHECK() $? "installing mongodb"
+
+systemctl enable mongod 
+CHECK() $? "enabaling mongodb"
+
+systemctl start mongod
+CHECK() $? "started mongodb"
+
+#changing mongobd host ip using sed(streamline editor ) very powerful
+
+sed -i 's/127.0.0.1 to 0.0.0.0g' /etc/mongod.conf
+CHECK() $? " setting remote access to mongodb"
+
+systemctl restart mongod 
+CHECK() $? "restarting mongodb"
--- readme.md
+++ readme.md
@@ -1,1 +1,15 @@
-ll
+one way to connect with remote repo
+
+create a repo remotely 
+
+
+then open local terminal 
+clone the remote repo to local using remote repo ssh
+
+use commands to keygen
+1. ssh-keygen -t ed25519 -C "your-email-id"
+2. publickey and private keys are generated
+3. keep the private key with you and place public key in github
+4. then go to terminal and do 
+      ssh -t git@github.com
+5. github will connect
